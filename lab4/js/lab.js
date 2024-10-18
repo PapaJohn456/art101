b9a4f45b8de0091baff27a94d9f1ac63323c681f
PapaJohn456 <gotham4069@gmail.com>
2024-10-17 18:24:22 -0700

    TTT

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -4,13 +4,127 @@
 
 // Constants
 
+//Set up List for answer conditions
+var Answer=[];
+
+//Result Conditions
+
+
+
+//Create References for buttons
+const Rock = document.getElementById("Rock")
+const Scissors = document.getElementById("Scissors")
+const Paper = document.getElementById("Paper")
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Functions checks win conditions and returns the result
+function DetermineWinner() {
+  // When Players Choice is Rock
+  if (Answer[0]=="Rock"){
+    if (Answer[1]=="Rock"){
+      return "The Bot Chose Rock: It's a TIE :("
+    }
+    else if (Answer[1]=="Paper"){
+      return "The Bot Chose Paper: You LOST:("
+    }
+    else return "The Bot Chose Scissors:YOU WIN:)"
+
+  }
+
+  //When Players Choice is Paper
+  if (Answer[0]=="Scissors"){
+    if (Answer[1]=="Scissors"){
+      return "The Bot Chose Scissors: It's a TIE:("
+    }
+    else if (Answer[1]=="Paper"){
+      return "The Bot Chose Paper: YOU WIN:)"
+    }
+    else return "The Bot Chose Rock: You LOST:("
+
+  }
+
+  //When Players Choice is Scissors
+  if (Answer[0]=="Paper"){
+    if (Answer[1]=="Paper"){
+      return "The Bot Chose Paper: It's a TIE:("
+    }
+    else if (Answer[1]=="Scissors"){
+      return "The Bot Chose Scissors: You LOST:("
+    }
+    else return "The Bot Chose Rock: YOU WIN:)"
+
+  }
 }
+
+//This function will choose the Bots Choice
+function BotChoice(num){
+  //Picks random number from 0-2 *3 possibilities*
+  num= Math.floor(Math.random() * 3);
+  if (num==0){
+    return "Rock"
+  }
+  if(num==1){
+    return "Paper"
+
+  }
+  if(num==2){
+    return "Scissors"
+
+  }
+
+}
+
+//When the rock button is clicked 
+Rock.addEventListener("click", function(e){
+  
+  //Adds Rock into List
+  Answer.push("Rock");
+  //Makes Sure the lists doesn't exceed 2 inputs
+  if (Answer.length >2){
+    Answer=["Rock"]
+  }
+  //Adds the Computers Choice
+  Answer.push(BotChoice());
+  //Will Print Who Won
+  alert(DetermineWinner())
+
+});
+
+//When Scissors button is called
+Scissors.addEventListener("click", function(e){
+  
+  //Adds Rock into List
+  Answer.push("Scissors");
+  //Makes Sure the lists doesn't exceed 2 inputs
+  if (Answer.length >2){
+    Answer=["Scissors"]
+  }
+  //Adds the Computers Choice
+  Answer.push(BotChoice());
+  //Will Print Who Won
+
+  alert(DetermineWinner())
+
+});
+//When Paper button is Called
+Paper.addEventListener("click", function(e){
+  
+  //Adds Rock into List
+  Answer.push("Paper");
+  //Makes Sure the lists doesn't exceed 2 inputs
+  if (Answer.length >2){
+    Answer=["Paper"]
+  }
+  //Adds the Computers Choice
+  Answer.push(BotChoice());
+  //Will Print Who Won
+  alert(DetermineWinner())
+
+
+});
+
+
+
 
 function main() {
   console.log("Main function started.");
