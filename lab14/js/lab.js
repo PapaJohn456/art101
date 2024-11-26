4043f6ac9e5fb00fe50249415457e9674275abae
PapaJohn456 <gotham4069@gmail.com>
2024-11-25 21:56:40 -0800

    Update lab.js

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -17,54 +17,22 @@
   
 // add button to challenge section
 
-function sortHat() {
-  var answer=''
-  for (var i=0; i<=200; i++) {
-    
-    if (i%15==0 && i%21==0){
-      $("#output").append('<div class="text"><p>' + "FizzBuzzBoom!" + '</p></div>');
-    }
-    else if (i%15==0){ //3&5
-      $("#output").append('<div class="text"><p>' + "FizzBuzz!" + '</p></div>');
-    }
-    else if (i%21==0){ //3&7
-      $("#output").append('<div class="text"><p>' + "FizzBooM!" + '</p></div>');
-    }
-    else if (i%35==0){
-      $("#output").append('<div class="text"><p>' + "BuzzBoom!" + '</p></div>'); //5&7
-    }
-    else if(i%3==0){
-      $("#output").append('<div class="text"><p>' + "Fizz!" + '</p></div>');
-  
-    }
-    else if(i%5==0){
-      $("#output").append('<div class="text"><p>' + "Buzz!" + '</p></div>');
-  
-    }
-    else if(i%7==0){
-      $("#output").append('<div class="text"><p>' + "Boom!" + '</p></div>');
-  
-    }
-    else{
-      $("#output").append('<div class="text"><p>' + i + '</p></div>');
-    }
-  }
-  
-}
+
 
 // click listener for button
 $("#submit").click(function(){
-    let data= {
-        "answer": "yes",
-        "forced": false,
-        "image": "https://yesno.wtf/assets/yes/2.gif"
-      }
-      $("#output").append('<div class="text"><p>' + data.answer + '</p></div>');
-      $("#output").append("<img src='" + data.image + "'");
+  let data= {
+      "answer": "yes",
+      "forced": false,
+      "image": "https://yesno.wtf/assets/yes/2.gif"
+    }
+    $("#output").append('<h1>' + data.answer);
+    $("#output").append("<img src='" + data.image + "'>");
 
 
-    
+  
 });
+
 
 // append a new div to our output div
 
